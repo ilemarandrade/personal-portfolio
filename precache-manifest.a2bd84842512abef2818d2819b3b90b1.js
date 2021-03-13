52565f47a572a0f97844a5cbd6a7d7ce88276809
Ilemar Andrade <ilemarandrade@gmail.com>
2021-03-12 20:51:34 -0400

    Updates

---
+++ precache-manifest.a2bd84842512abef2818d2819b3b90b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb2e849268ebecd83dbbc8dabce2fe55",
+    "url": "/IlemarOficialV2.0/index.html"
+  },
+  {
+    "revision": "a1125eb4f6fe5cc8d139",
+    "url": "/IlemarOficialV2.0/static/css/main.cfefde64.chunk.css"
+  },
+  {
+    "revision": "c44421633cebc2c21ceb",
+    "url": "/IlemarOficialV2.0/static/js/2.70fe04bb.chunk.js"
+  },
+  {
+    "revision": "a1125eb4f6fe5cc8d139",
+    "url": "/IlemarOficialV2.0/static/js/main.5b783fcd.chunk.js"
+  },
+  {
+    "revision": "2fa83aef8c17f1f53333",
+    "url": "/IlemarOficialV2.0/static/js/runtime~main.b0c744c3.js"
+  }
+]);
