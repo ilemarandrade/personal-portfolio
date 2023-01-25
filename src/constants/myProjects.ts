cd8859667c8a4b22bdec830eed86e82b782ef350
Ilemar <ilemarandrade@gmail.com>
2023-01-24 20:03:33 -0400

    feat: added constant with my projects

---
+++ src/constants/myProjects.ts
@@ -0,0 +1,76 @@
+import pelis from "@/assets/images/pelis.png";
+
+const myProjects = [
+    
+  {
+    name: "Movie Search",
+    link: "/works/nameOfprojectIs",
+    environment: "REACT APP WEB",
+    img: pelis,
+    codesandboxLink:
+      "https://codesandbox.io/embed/busqueda-de-peliculas-fzzh2?fontsize=14&hidenavigation=1&theme=dark&view=split&editorsize=50",
+  },
+  {
+    name: "Wallet",
+    link: "/works/nameOfprojectIs",
+    environment: "REACT APP WEB",
+    img: pelis,
+    codesandboxLink:
+      "https://codesandbox.io/embed/billetera-zutg6?fontsize=14&hidenavigation=1&theme=dark&view=split&editorsize=50",
+  },
+  {
+    name: "Pomodoro Clock",
+    link: "/works/nameOfprojectIs",
+    environment: "REACT APP WEB",
+    img: pelis,    
+    codesandboxLink:
+      "https://codesandbox.io/embed/reloj-pomodoro-5tgjr?fontsize=14&hidenavigation=1&theme=dark&view=split&editorsize=50",
+  },
+  {
+    name: "Drum Machine",
+    link: "/works/nameOfprojectIs",
+    environment: "REACT APP WEB",
+    img: pelis,   
+    codesandboxLink:
+      "https://codesandbox.io/embed/drum-pad-q8dds?fontsize=14&hidenavigation=1&theme=dark&view=split&editorsize=50",
+  },
+  /*{
+        name: "Memory Game",
+        link: "/works/nameOfprojectIs",
+        img:
+ pelis    },*/
+  {
+    name: "Calculator",
+    link: "/works/nameOfprojectIs",
+    environment: "REACT APP WEB",
+    img: pelis,
+    codesandboxLink:
+      "https://codesandbox.io/embed/calculadora-5kl0f?fontsize=14&hidenavigation=1&theme=dark&view=split&editorsize=50",
+  },
+  {
+    name: "Landing Page",
+    link: "/works/nameOfprojectIs",
+    environment: "REACT APP WEB",
+    img: pelis,
+    codesandboxLink:
+      "https://codesandbox.io/embed/landing-page-edi-react-bq2hq?fontsize=14&hidenavigation=1&theme=dark&view=split&editorsize=50",
+  },
+  {
+    name: "New Quote",
+    link: "/works/nameOfprojectIs",
+    environment: "REACT APP WEB",
+    img: pelis,    
+    codesandboxLink:
+      "https://codesandbox.io/embed/mensajes-con-autores-evet4?fontsize=14&hidenavigation=1&theme=dark&view=split&editorsize=50",
+  },
+  {
+    name: "MarkDown",
+    link: "/works/nameOfprojectIs",
+    environment: "REACT APP WEB",
+    img: pelis,    
+    codesandboxLink:
+      "https://codesandbox.io/embed/markedjs-51q3x?fontsize=14&hidenavigation=1&theme=dark&view=split&editorsize=50",
+  },
+];
+
+export default myProjects;
