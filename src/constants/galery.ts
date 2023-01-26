c0bbcd82e9207a162ddcd99ebb045df73fd37ce5
Ilemar <ilemarandrade@gmail.com>
2023-01-26 10:37:05 -0400

    feat: added image to galery, constant wit images and box props, and created BoxImage Component

---
+++ src/constants/galery.ts
@@ -0,0 +1,59 @@
+const galery = [
+  {
+    url: "/images/galery/1.jpeg",
+    propsGrid: {
+      xs: 12,
+      sm: 6,
+      md: 4,
+    },
+  },
+  {
+    url: "/images/galery/2.jpeg",
+    propsGrid: {
+      xs: 12,
+      sm: 6,
+      md: 4,
+    },
+  },
+  {
+    url: "/images/galery/3.jpeg",
+    propsGrid: {
+      xs: 12,
+      sm: 6,
+      md: 4,
+    },
+  },
+  {
+    url: "/images/galery/4.jpeg",
+    propsGrid: {
+      xs: 12,
+      sm: 6,
+      md: 4,
+    },
+  },
+  {
+    url: "/images/galery/5.jpeg",
+    propsGrid: {
+      xs: 12,
+      sm: 6,
+      md: 8,
+    },
+  },
+  {
+    url: "/images/galery/7.jpeg",
+    propsGrid: {
+      xs: 12,
+      sm: 6,
+      md: 4,
+    },
+  },
+  {
+    url: "/images/galery/6.jpeg",
+    propsGrid: {
+      xs: 12,
+      sm: 6,
+      md: 8,
+    },
+  },
+];
+export default galery;
